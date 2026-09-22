86a540e60cf18e53da8bd9b08522c34d79f7bd29
MelaMele <melamele06@gmail.com>
2026-09-22 22:03:26 +0300

    Create Employee model with fillable attributes

---
+++ app/Models/Employee.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
+
+class Employee extends Model
+{
+    protected $fillable = [
+        'full_name',
+        'phone_number',
+        'access_code',
+        'department',
+        'position',
+        'device_id',
+        'is_active',
+    ];
+
+    public function attendances(): HasMany
+    {
+        return $this->hasMany(Attendance::class);
+    }
+
+    public function leaveRequests(): HasMany
+    {
+        return $this->hasMany(LeaveRequest::class);
+    }
+}
