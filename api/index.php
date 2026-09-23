b0f46baa92277b19c40e40562747fad72ff93f36
MelaMele <melamele06@gmail.com>
2026-09-23 22:00:56 +0300

    Setup temporary storage directories for Vercel
    Add temporary storage directory setup for Vercel serverless environment.

--- api/index.php
+++ api/index.php
@@ -1,3 +1,22 @@
 <?php
 
+// Vercel Serverless ጊዜያዊ ፎልደሮችን ማዘጋጀት
+$storageDirs = [
+    '/tmp/storage/framework/views',
+    '/tmp/storage/framework/cache',
+    '/tmp/storage/framework/sessions',
+    '/tmp/storage/logs',
+    '/tmp/bootstrap/cache'
+];
+
+foreach ($storageDirs as $dir) {
+    if (!is_dir($dir)) {
+        mkdir($dir, 0755, true);
+    }
+}
+
+// Environment ለ Vercel ማስተካከያ
+$_ENV['APP_STORAGE'] = '/tmp/storage';
+$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+
 require __DIR__ . '/../public/index.php';
