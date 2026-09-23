284c235dcce49db78f4e7ea0060d6b7cf4d6fb7d
MelaMele <melamele06@gmail.com>
2026-09-23 22:20:28 +0300

    Refactor storage directory setup for Vercel

--- api/index.php
+++ api/index.php
@@ -1,7 +1,7 @@
 <?php
 
-// Vercel Serverless ጊዜያዊ ፎልደሮችን ማዘጋጀት
-$storageDirs = [
+// Vercel Serverless የሚጻፉባቸውን ጊዜያዊ ፎልደሮች ማዘጋጀት
+$dirs = [
     '/tmp/storage/framework/views',
     '/tmp/storage/framework/cache',
     '/tmp/storage/framework/sessions',
@@ -9,14 +9,23 @@ $storageDirs = [
     '/tmp/bootstrap/cache'
 ];
 
-foreach ($storageDirs as $dir) {
+foreach ($dirs as $dir) {
     if (!is_dir($dir)) {
         mkdir($dir, 0755, true);
     }
 }
 
-// Environment ለ Vercel ማስተካከያ
-$_ENV['APP_STORAGE'] = '/tmp/storage';
-$_ENV['VIEW_COMPILED_PATH'] = '/tmp/storage/framework/views';
+// Laravel bootstrap cache pathን ወደ /tmp ማዞሪያ
+putenv('APP_PACKAGES_CACHE=/tmp/bootstrap/cache/packages.php');
+putenv('APP_SERVICES_CACHE=/tmp/bootstrap/cache/services.php');
+putenv('APP_CONFIG_CACHE=/tmp/bootstrap/cache/config.php');
+putenv('APP_ROUTES_CACHE=/tmp/bootstrap/cache/routes.php');
+putenv('APP_EVENTS_CACHE=/tmp/bootstrap/cache/events.php');
+
+$_ENV['APP_PACKAGES_CACHE'] = '/tmp/bootstrap/cache/packages.php';
+$_ENV['APP_SERVICES_CACHE'] = '/tmp/bootstrap/cache/services.php';
+$_ENV['APP_CONFIG_CACHE'] = '/tmp/bootstrap/cache/config.php';
+$_ENV['APP_ROUTES_CACHE'] = '/tmp/bootstrap/cache/routes.php';
+$_ENV['APP_EVENTS_CACHE'] = '/tmp/bootstrap/cache/events.php';
 
 require __DIR__ . '/../public/index.php';
